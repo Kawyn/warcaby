3ddffb288b8c049b6ae47897d2c7f3c5ed825ca7
Bbusinka <valeriyaloychik@gmail.com>
2023-01-03 14:34:46 +0100

    Add search client

---
+++ src/main/java/checkers/Client/Interface/Controllers/SearchController.java
@@ -0,0 +1,13 @@
+package checkers.Client.Interface.Controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+
+public class SearchController {
+    @FXML
+    private ProgressBar progres;
+
+    @FXML
+    void initialize() {
+
+    }
+}
