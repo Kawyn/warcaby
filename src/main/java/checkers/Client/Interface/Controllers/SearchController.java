72127658325706a271902d9256da5562da33b3cc
Bbusinka <valeriyaloychik@gmail.com>
2023-01-07 16:11:24 +0100

    Podłaczenie serwera i klienta do systemu

--- src/main/java/checkers/Client/Interface/Controllers/SearchController.java
+++ src/main/java/checkers/Client/Interface/Controllers/SearchController.java
@@ -1,13 +1,72 @@
 package checkers.Client.Interface.Controllers;
+
+import checkers.Server.Client;
+import checkers.Server.Serwer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
+import java.util.function.UnaryOperator;
+
 
 public class SearchController {
+    private Serwer serwer;
+    private Client client;
     @FXML
-    private ProgressBar progres;
+    private Button next;
+
+    @FXML
+    private TextField numeric;
+
+    @FXML
+    private ComboBox<String> typ;
 
     @FXML
     void initialize() {
+        LoadList();
+        checkInt();
+        next.setOnAction(event -> {
+            if (numeric.getText().length() < 4) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Błąd!");
+                alert.setHeaderText(null);
+                alert.setContentText("Dane niepoprawne! Sprawdż jeszcze raz! Muszą być 4 cyfry.");
+                alert.showAndWait();
+            }
+            if (getWariant()==0){
+                serwer = new Serwer(Integer.parseInt(numeric.getText()));
+                serwer.run();
+            }
+            else {
+                client.initConnection(Integer.parseInt(numeric.getText()));
+            }
 
+        });
     }
+
+    private void LoadList() {
+        ObservableList<String> list = FXCollections.observableArrayList("Stworzyć lobby", "Dołącz do lobby");
+        typ.setItems(list);
+        typ.setValue("Stworzyć lobby");
+    }
+
+    public void checkInt() {
+        numeric.setOnKeyTyped(event -> {
+            int maxCharacters = 3;
+            if (numeric.getText().length() > maxCharacters) event.consume();
+        });
+        UnaryOperator<TextFormatter.Change> integerFilter = change -> {
+            String input = change.getText();
+            if (input.matches("[0-9]*")) {
+                return change;
+            }
+            return null;
+        };
+        numeric.setTextFormatter(new TextFormatter<String>(integerFilter));
+    }
+
+    public int getWariant(){
+        return typ.getSelectionModel().getSelectedIndex();
+    }
+
 }
